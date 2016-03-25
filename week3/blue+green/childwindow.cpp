de8234a8e2404d4a9a5d6a7b393d278e4cfa797f
wyldecat <skan1543@gmail.com>
2016-03-25 13:51:22 +0900

    Revert "03/21"
    This reverts commit af8f0b38ffdedbdde18d9db5d2efc1c91c3ab0e5.

--- week3/blue+green/childwindow.cpp
+++ week3/blue+green/childwindow.cpp
@@ -12,26 +12,11 @@ ChildWindow::ChildWindow(OPENFILENAME _OFN)
 }
 LRESULT CALLBACK ChildWndProc(HWND hWnd,UINT iMessage,WPARAM wParam,LPARAM lParam)
 {
-    PAINTSTRUCT ps;
     switch(iMessage)
     {
-    case WM_MOVE:
-        RECT r,r2;
-        GetWindowRect(hWnd,&r);
-        GetWindowRect(window_main.get_handle(),&r2);
-        if(r.left<r2.left)MoveWindow(hWnd,0,r.top-r2.top-50,700,500,true);  // 자식 윈도우가 위,왼쪽으로 나가지 않도록..!근본적인 해결책은 아닌것같다.. 중요한건 아니니 이정도로만..!
-        else if(r.top<r2.top+80)MoveWindow(hWnd,r.left-r2.left-10,30,700,500,true);
-    case WM_PAINT: // 더블버퍼링     기법 http://hardprogram.tistory.com/entry/API-더블버퍼링-기법 참고했당.
-        RECT crt;
-        GetClientRect(hWnd, &crt);
-        HDC hdc = BeginPaint(hWnd, &ps);
-        HDC hMemDC = CreateCompatibleDC(hdc);
-        HBITMAP hBitmap = CreateCompatibleBitmap(hdc, crt.right, crt.bottom);
-        HBITMAP OldBitmap = (HBITMAP)SelectObject(hMemDC, hBitmap);
-        BitBlt(hdc, 0,0, crt.right, crt.bottom, hMemDC, 0,0, SRCCOPY);
-        DeleteObject(SelectObject(hMemDC, OldBitmap));
-        DeleteDC(hMemDC);
-        EndPaint(hWnd, &ps);
+    //WM_CREATE: opengl 디스플레이 추가
+    case WM_PAINT:
+        InvalidateRect(hWnd,NULL,true);
         return 0;
     }
     return (DefWindowProc(hWnd,iMessage,wParam,lParam));
@@ -53,5 +38,5 @@ void ChildWindow::set_window()
     win.lpszClassName="Child";
     win.style=CS_HREDRAW|CS_VREDRAW;
     RegisterClass(&win);
-    CreateWindowEx(0,"Child","Child",WS_CHILD|WS_VISIBLE|WS_SYSMENU|WS_CAPTION|WS_THICKFRAME |WS_CLIPCHILDREN ,10,30,700,500,window_main.get_handle(),(HMENU)NULL,g_hInst,NULL);
+    CreateWindowEx(0,"Child","Child",WS_CHILD|WS_VISIBLE|WS_SYSMENU|WS_CAPTION|WS_THICKFRAME,10,30,700,500,window_main.get_handle(),(HMENU)NULL,g_hInst,NULL);
 }
